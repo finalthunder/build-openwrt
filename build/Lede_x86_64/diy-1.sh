624581ab272fab66b1a695165f00ce0d7aabf45c
finalthunder <68462377+finalthunder@users.noreply.github.com>
2021-03-05 16:22:00 +0800

    Update diy-1.sh

--- build/Lede_x86_64/diy-1.sh
+++ build/Lede_x86_64/diy-1.sh
@@ -6,3 +6,4 @@ git clone https://github.com/garypang13/luci-app-bypass package/luci-app-bypass
 svn co https://github.com/garypang13/openwrt-packages/trunk/lua-maxminddb
 find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-redir/shadowsocksr-libev-alt/g' {}
 find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-server/shadowsocksr-libev-server/g' {}
+git clone  https://github.com/ITdesk01/jd_openwrt_script.git package/jd_openwrt_script./scripts/feeds update && ./scripts/feeds install
